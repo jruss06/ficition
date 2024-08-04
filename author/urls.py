0394a486857014aef4c04d323e858b826ff0db27
james <jajrusso@gmail.com>
2024-08-04 19:16:00 -0400

    viewauthor

--- author/urls.py
+++ author/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.profile, name="profile"),
+    path("view/<int:author_id>", views.viewauthor, name="viewauthor"),
 ]
--- author/views.py
+++ author/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from story.models import Story, Chapter
+from django.contrib.auth.models import Permission, User
 
 def profile(request):
     story_list = Story.objects.filter(user_id=request.user.id)
@@ -15,6 +16,7 @@ def profile(request):
 def viewauthor(request, author_id):
     story_list = Story.objects.filter(user_id=author_id)
     context = {
+         "user": User.objects.get(pk=author_id),
          "story_list": story_list
     }
-    return render(request, "author/viewauthor.html", context)
+    return render(request, "author/view.html", context)
