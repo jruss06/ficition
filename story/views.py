23655b32339c21b30ff29ebb8112842055e90c45
james <jajrusso@gmail.com>
2024-08-05 21:03:33 -0400

    add chapter select

--- story/views.py
+++ story/views.py
@@ -15,7 +15,8 @@ def index(request):
 def show(request, story_id, chapter_num=1):
     story = get_object_or_404(Story, pk=story_id)
     chapter = get_object_or_404(Chapter, story_id=story_id, order=chapter_num)
-    return render(request, "story/show.html", {"story": story, "chapter": chapter})
+    chapter_list = Chapter.objects.filter(story_id = story_id)
+    return render(request, "story/show.html", {"story": story, "chapter": chapter, "chapter_list": chapter_list})
 
 def addstory(request):
     return render(request, "story/addstory.html")
