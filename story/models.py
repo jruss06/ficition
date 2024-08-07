3d844c23701dfd559a1d164ec98e379c099d1dc1
James Russo <russoj@sharpsec.com>
2024-08-07 13:21:06 -0400

    many to many on story

--- story/models.py
+++ story/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from tags.models import Tag
+
 class Story(models.Model):
     class Meta:
         verbose_name = 'Story'
@@ -10,6 +12,7 @@ class Story(models.Model):
     added_date = models.DateTimeField("date added", auto_now_add=True)
     update_date = models.DateTimeField("date updated", auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
     def __str__(self):
         return self.title
 
